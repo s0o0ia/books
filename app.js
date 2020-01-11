0c4792316642ec5f9ba56d7f1221739361f7ad66
Romashkaaaa <teygame7@gmail.com>
2020-01-11 14:48:41 +0200

    first commit

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 1300;
+const home = require("./routes");
+const exphbs = require("express-handlebars");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname: "hbs"
+});
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+app.use(express.static(__dirname + "/web"));
 
 
-
-const routes = require("./routes");
-
-app.use(routes);
+app.use(home);
 
 
 app.listen(PORT,() => {
-    console.log("server working")
+    console.log("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ server working*:･ﾟ✧")
 });
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,27 @@ const { Router } = require("express");
 const router = Router();
 const path = require("path");
 
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname,"../web", "index.html"));
+// });
+
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"../web", "index.html"));
+  res.render("index");
 });
-
-
+router.get("/product", (req, res) => {
+  res.render("product");
+});
+router.get("/account", (req, res) => {
+  res.render("account");
+});
+router.get("/contact", (req, res) => {
+  res.render("contact");
+});
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.get("/single", (req, res) => {
+  res.render("single");
+});
 
 module.exports = router;
