5ccd2d246f5293c160c45ad734cb698b42f8f0a5
s0o0ia <sofishandala13@gmail.com>
2020-02-26 11:24:59 +0200

    version1.5

--- routes/index.js
+++ routes/index.js
@@ -18,23 +18,23 @@ const sql = `SELECT * FROM products`;
  
 connection.query(sql, function(err, results) {
     if(err) console.log(err);
-    // console.log(results);
-    var el='' ;
-    results.forEach(function(item){
-     return el += `<div class="col-md-4 grid-product-in">	
-                    <div class=" product-grid">	
-                      <a href="/single"><img class="img-responsive " src="images/pr.png" alt=""></a>		
-                      <div class="shoe-in">
-                        <h6><a href="/single">${results.name_book }</a></h6>
-                        <p>${results.description_book}</p>
-                        <label>$${results.price_book }</label>
-                        <a href="/single" class="store">FIND A STORE</a>
-                      </div>
-                      <b class="plus-on">+</b>
-                    </div>	
-                    </div>`
-    })
-    document.querySelector('.product-top').innerHTML = el;
+    console.log(results);
+    // var el='' ;
+    // results.forEach(function(item){
+    //  return el += `<div class="col-md-4 grid-product-in">	
+    //                 <div class=" product-grid">	
+    //                   <a href="/single"><img class="img-responsive " src="images/pr.png" alt=""></a>		
+    //                   <div class="shoe-in">
+    //                     <h6><a href="/single">${results.name_book }</a></h6>
+    //                     <p>${results.description_book}</p>
+    //                     <label>$${results.price_book }</label>
+    //                     <a href="/single" class="store">FIND A STORE</a>
+    //                   </div>
+    //                   <b class="plus-on">+</b>
+    //                 </div>	
+    //                 </div>`
+    // })
+    // document.querySelector('.product-top').innerHTML = el;
 });
 
 
